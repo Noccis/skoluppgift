9a20df58616db264c103ec73761db24a5322a423
HenrikSjoe <henrik.sjogren@iths.se>
2021-10-05 13:27:32 +0200

    ssdgsg

--- app/src/main/java/com/example/helpwithpicturesapp/MainActivity.kt
+++ app/src/main/java/com/example/helpwithpicturesapp/MainActivity.kt
@@ -7,5 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    } // Hej Hej hej
+    } // Hej Hej hej lkhbkljb
 }
