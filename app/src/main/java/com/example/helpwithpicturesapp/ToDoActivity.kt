12716bee3dd46369272f32b8ac248475d948628f
Jesper Söderling <jesper.soderling@me.com>
2021-10-18 10:42:51 +0200

    .

--- app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
+++ app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.core.view.get
@@ -19,8 +20,8 @@ class ToDoActivity : AppCompatActivity() {
     lateinit var addButton: FloatingActionButton
     lateinit var recyclerView: RecyclerView
     val action = mutableListOf<Actions>()
-    lateinit var db : FirebaseFirestore
-    lateinit var myAdapter : ActionsRecycleViewAdapter
+    lateinit var db: FirebaseFirestore
+    lateinit var myAdapter: ActionsRecycleViewAdapter
     val TAG = "!!!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,62 +43,64 @@ class ToDoActivity : AppCompatActivity() {
 
         addButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         addButton.setOnClickListener {
-            val intent = Intent(this,UserCreateAndEditActivity::class.java)
+            val intent = Intent(this, UserCreateAndEditActivity::class.java)
             startActivity(intent)
         }
+        getUserUploadPicture()
 
     }
 
-        fun EventChangeListener() {
+    fun EventChangeListener() {
 
-            db = FirebaseFirestore.getInstance()
-            db.collection("Actions").orderBy("order", Query.Direction.ASCENDING).
-                    addSnapshotListener(object : EventListener<QuerySnapshot>{
+        db = FirebaseFirestore.getInstance()
+        db.collection("Actions").orderBy("order", Query.Direction.ASCENDING)
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
 
-                        override fun onEvent(
-                            value: QuerySnapshot?,
-                            error: FirebaseFirestoreException?
-                        ) {
-                            if (error != null) {
-                                Log.d("Firestore error", error.message.toString())
-                                return
-                            }
+                override fun onEvent(
+                    value: QuerySnapshot?,
+                    error: FirebaseFirestoreException?
+                ) {
+                    if (error != null) {
+                        Log.d("Firestore error", error.message.toString())
+                        return
+                    }
 
-                            for ( dc: DocumentChange in value?.documentChanges!!){
-                                if (dc.type == DocumentChange.Type.ADDED){
-                                    action.add(dc.document.toObject(Actions::class.java))
-                                }
-                            }
-                           myAdapter.notifyDataSetChanged()
+                    for (dc: DocumentChange in value?.documentChanges!!) {
+                        if (dc.type == DocumentChange.Type.ADDED) {
+                            action.add(dc.document.toObject(Actions::class.java))
                         }
-                    })
+                    }
+                    myAdapter.notifyDataSetChanged()
+                }
+            })
 
-        }
+    }
 
     fun getUserUploadPicture() {
+
+        val actionsRef = db.collection("Actions")
+
         val userAction1 = Actions(
             "UserAction",
-            "https://firebasestorage.googleapis.com/v0/b/helpwithpicturesapp-f9c12.appspot.com/o/UploadedPictures%2F2021_10_15_07_15_42?alt=media&token=0eccc707-adf6-4574-9152-f327ec007ac9",
+            "https://firebasestorage.googleapis.com/v0/b/helpwithpicturesapp-f9c12.appspot.com/o/UploadedPictures%2F2021_10_15_12_45_45?alt=media&token=848fb693-32fa-4632-9f93-a04cef2d50e9",
             false,
-            "")
+            "Soffa"
+        )
+        db.collection("Actions").document("UploadedPictures").set(userAction1)
 
-        val db = FirebaseFirestore.getInstance()
-        val userUploadAction = db.collection("Actions").document("UserUpload").set(userAction1)
-        Log.d(TAG, "1. onCreate: Gör det")
-        userUploadAction.get().addOnSuccessListener { doucmentSnapShot -> // doucumentSnapShot == ger Listener ett namn
-            for (document in doucmentSnapShot.documents) { // Går igenom ett dokument i taget
+            actionsRef.get().addOnSuccessListener { doucmentSnapShot -> // doucumentSnapShot == ger Listener ett namn
+                for (document in doucmentSnapShot.documents) { // Går igenom ett dokument i taget
 
-                val newUserAction = document.toObject(Actions::class.java) // Addar nytt item till ItemFolder i Firebase
+                    val newUserAction =
+                        document.toObject(Actions::class.java) // Addar nytt item till ItemFolder i Firebase
 
-                if(newUserAction != null) {
-                    action.add(newUserAction)
-                    Log.d(TAG, "2. onCreate: ${newUserAction}")
+                    if (newUserAction != null) {
+                        action.add(newUserAction)
+                    }
                 }
+
             }
-            Log.d(TAG, "3. onCreate: efter get")
 
-            Log.d(TAG,"onCreate: ${action.size}")
     }
-
 }
 
