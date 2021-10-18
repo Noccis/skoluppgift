5708ce81bff0d7d217e90d259f5d45be22168647
zadnan82 <zainab.adnan@iths.se>
2021-10-18 19:49:55 +0200

    Fixat variabeln i databasen

--- app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
+++ app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
@@ -26,15 +26,14 @@ class ToDoActivity : AppCompatActivity() {
     val action = mutableListOf<Actions>()
     lateinit var db: FirebaseFirestore
     lateinit var myAdapter: ActionsRecycleViewAdapter
-    val TAG = "!!!"
-    var decision = 0
+    var decision = ""
     lateinit var dayTextView : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
 
         dayTextView = findViewById(R.id.dayTextView)
-        decision = intent.getIntExtra(Constants.DAY_CHOSEN, 0)
+        decision = intent.getStringExtra(Constants.DAY_CHOSEN).toString()
 
 
         recyclerView = findViewById(R.id.recyclerView)
@@ -47,9 +46,6 @@ class ToDoActivity : AppCompatActivity() {
 
         EventChangeListener()
 
-
-
-
         addButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         addButton.setOnClickListener {
             val intent = Intent(this, UserCreateAndEditActivity::class.java)
@@ -58,179 +54,40 @@ class ToDoActivity : AppCompatActivity() {
        // getUserUploadPicture()
 
     }
-
-
-
-
         fun EventChangeListener() {
 
-        if (decision == 1) {
-            dayTextView.text = "Måndag"
-            dayTextView.setBackgroundColor(Color.parseColor("#92d051"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("monday").collection("monday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else if (decision == 2) {
-
-            dayTextView.text = "Tisdag"
-            dayTextView.setBackgroundColor(Color.parseColor("#92cddd"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("tuesday").collection("tuesday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else  if (decision == 3) {
-
-            dayTextView.text = "Onsdag"
-            dayTextView.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("wednesday").collection("wednesday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else if (decision == 4) {
-
-            dayTextView.text = "Torsdag"
-            dayTextView.setBackgroundColor(Color.parseColor("#c88f58"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("thursday").collection("thursday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else  if (decision == 5) {
-
-            dayTextView.text = "Fredag"
-            dayTextView.setBackgroundColor(Color.parseColor("#ffff6d"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("friday").collection("friday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else if (decision == 6) {
-
-            dayTextView.text = "Lördag"
-            dayTextView.setBackgroundColor(Color.parseColor("#faa8d8"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("saturday").collection("saturday")
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-        } else  if (decision == 7) {
-
-            dayTextView.text = "Söndag"
-            dayTextView.setBackgroundColor(Color.parseColor("#ff4342"));
-            db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document("sunday").collection("sunday")
+            when (decision) {
+                "monday" -> {
+                    dayTextView.text = "Måndag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#92d051"));
+                }
+                "tuesday" -> {
+                    dayTextView.text = "Tisdag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#92cddd"));
+                }
+                "wednesday" -> {
+                    dayTextView.text = "Onsdag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+                }
+                "thursday" -> {
+                    dayTextView.text = "Torsdag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#c88f58"));
+                }
+                "friday" -> {
+                    dayTextView.text = "Fredag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#ffff6d"));
+                }
+                "saturday" -> {
+                    dayTextView.text = "Lördag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#faa8d8"));
+                }
+                "sunday" -> {
+                    dayTextView.text = "Söndag"
+                    dayTextView.setBackgroundColor(Color.parseColor("#ff4342"));
+                }
+            }
+                db = FirebaseFirestore.getInstance()
+            db.collection("Weekday").document(decision).collection(decision)
                 .orderBy("order", Query.Direction.ASCENDING)
                 .addSnapshotListener(object : EventListener<QuerySnapshot> {
 
@@ -253,8 +110,6 @@ class ToDoActivity : AppCompatActivity() {
                 })
 
         }
-
-    }
 
 
 }
