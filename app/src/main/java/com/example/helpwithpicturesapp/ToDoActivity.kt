269f838a92a3f82fcb796e5903828d168b6a3d80
zadnan82 <zainab.adnan@iths.se>
2021-10-26 14:59:08 +0200

    update

--- app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
+++ app/src/main/java/com/example/helpwithpicturesapp/ToDoActivity.kt
@@ -28,18 +28,19 @@ class ToDoActivity : AppCompatActivity() {
     lateinit var db: FirebaseFirestore
     lateinit var myAdapter: ActionsRecycleViewAdapter
     var decision = ""
-    lateinit var dayTextView : TextView
-    lateinit var lockButton : ImageView
-    lateinit var passCard : CardView
-    lateinit var editPassword : EditText
+    lateinit var dayTextView: TextView
+    lateinit var lockButton: ImageView
+    lateinit var passCard: CardView
+    lateinit var editPassword: EditText
     private var longAnimationDuration: Int = 2000
-    lateinit var lock : Button
-    lateinit var unlock : Button
+    lateinit var lock: Button
+    lateinit var unlock: Button
     var password = ""
-    lateinit var close : ImageView
+    lateinit var close: ImageView
     lateinit var rewardImageView: ImageView
     private var shortAnimationDuration: Int = 400
-    lateinit var deletedCard : Actions
+    lateinit var deletedCard: Actions
+    var locked : Boolean  = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +48,7 @@ class ToDoActivity : AppCompatActivity() {
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
+        Log.d("!!!", " början $locked")
         password = intent.getStringExtra(Constants.PASSWORD).toString()
         editPassword = findViewById(R.id.editPassword)
         passCard = findViewById(R.id.passCard)
@@ -76,114 +78,124 @@ class ToDoActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        myAdapter = ActionsRecycleViewAdapter(this, action , decision, password)
+        myAdapter = ActionsRecycleViewAdapter(this, action, decision, password)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = myAdapter
         EventChangeListener()
+
 
         val itemTouchHelper = ItemTouchHelper(simpleCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
     }
 
-        var simpleCallback = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP
-            .or(ItemTouchHelper.DOWN),ItemTouchHelper.LEFT){
+    var simpleCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP
+            .or(ItemTouchHelper.DOWN), ItemTouchHelper.LEFT
+    ) {
 
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                if ( addButton.visibility == View.VISIBLE) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            if (addButton.visibility == View.VISIBLE) {
                 var startPosition = viewHolder.adapterPosition
                 var endPosition = target.adapterPosition
                 Collections.swap(action, startPosition, endPosition)
-                recyclerView.adapter?.notifyItemMoved(startPosition,endPosition)
+                recyclerView.adapter?.notifyItemMoved(startPosition, endPosition)
                 return true
             } else return false
-            }
+        }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int ) {
 
-                var position = viewHolder.adapterPosition
-                when (direction ){
-                    ItemTouchHelper.LEFT -> {
-                        deletedCard= action.get(position)
-                        action.removeAt(position)
-                        myAdapter.notifyItemRemoved(position)
-                        Snackbar.make(recyclerView, "Uppgiften är borttagen", Snackbar.LENGTH_LONG)
-                            .setAction("Ångra", View.OnClickListener {
+            Log.d("!!!", " först $locked")
+            if (locked){
+
+                return }
+
+            var position = viewHolder.adapterPosition
+            when (direction) {
+                ItemTouchHelper.LEFT -> {
+                    deletedCard = action.get(position)
+                    action.removeAt(position)
+                    myAdapter.notifyItemRemoved(position)
+                    Snackbar.make(recyclerView, "Uppgiften är borttagen", Snackbar.LENGTH_LONG)
+                        .setAction("Ångra", View.OnClickListener {
                             action.add(position, deletedCard)
                             myAdapter.notifyItemInserted(position)
                         }).show()
-                        }
                 }
             }
-
-    }
-        fun EventChangeListener() {
-            when (decision) {
-                "monday" -> {
-                    dayTextView.text = "Måndag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#92d051"));
-                }
-                "tuesday" -> {
-                    dayTextView.text = "Tisdag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#92cddd"));
-                }
-                "wednesday" -> {
-                    dayTextView.text = "Onsdag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
-                }
-                "thursday" -> {
-                    dayTextView.text = "Torsdag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#c88f58"));
-                }
-                "friday" -> {
-                    dayTextView.text = "Fredag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#ffff6d"));
-                }
-                "saturday" -> {
-                    dayTextView.text = "Lördag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#faa8d8"));
-                }
-                "sunday" -> {
-                    dayTextView.text = "Söndag"
-                    dayTextView.setBackgroundColor(Color.parseColor("#ff4342"));
-                }
-            }
-                db = FirebaseFirestore.getInstance()
-            db.collection("Weekday").document(decision).collection(decision)
-                .orderBy("order", Query.Direction.ASCENDING)
-                .addSnapshotListener(object : EventListener<QuerySnapshot> {
-
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null) {
-                            Log.d("Firestore error", error.message.toString())
-                            return
-                        }
-
-                        for (dc: DocumentChange in value?.documentChanges!!) {
-                            if (dc.type == DocumentChange.Type.ADDED) {
-                                action.add(dc.document.toObject(Actions::class.java))
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
         }
 
-    fun reward () {
+    }
+
+    fun EventChangeListener() {
+        when (decision) {
+            "monday" -> {
+                dayTextView.text = "Måndag"
+                dayTextView.setBackgroundColor(Color.parseColor("#92d051"));
+            }
+            "tuesday" -> {
+                dayTextView.text = "Tisdag"
+                dayTextView.setBackgroundColor(Color.parseColor("#92cddd"));
+            }
+            "wednesday" -> {
+                dayTextView.text = "Onsdag"
+                dayTextView.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+            }
+            "thursday" -> {
+                dayTextView.text = "Torsdag"
+                dayTextView.setBackgroundColor(Color.parseColor("#c88f58"));
+            }
+            "friday" -> {
+                dayTextView.text = "Fredag"
+                dayTextView.setBackgroundColor(Color.parseColor("#ffff6d"));
+            }
+            "saturday" -> {
+                dayTextView.text = "Lördag"
+                dayTextView.setBackgroundColor(Color.parseColor("#faa8d8"));
+            }
+            "sunday" -> {
+                dayTextView.text = "Söndag"
+                dayTextView.setBackgroundColor(Color.parseColor("#ff4342"));
+            }
+        }
+        db = FirebaseFirestore.getInstance()
+        db.collection("Weekday").document(decision).collection(decision)
+            .orderBy("order", Query.Direction.ASCENDING)
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
+
+                override fun onEvent(
+                    value: QuerySnapshot?,
+                    error: FirebaseFirestoreException?
+                ) {
+                    if (error != null) {
+                        Log.d("Firestore error", error.message.toString())
+                        return
+                    }
+
+                    for (dc: DocumentChange in value?.documentChanges!!) {
+                        if (dc.type == DocumentChange.Type.ADDED) {
+                            action.add(dc.document.toObject(Actions::class.java))
+                        }
+                    }
+                    myAdapter.notifyDataSetChanged()
+                }
+            })
+
+    }
+
+    fun reward() {
         fadeIn()
         rewardSound()
         Handler(Looper.getMainLooper()).postDelayed({
             hideReward()
         }, 1000)
     }
+
     private fun fadeIn() {
         rewardImageView.apply {
             alpha = 0f
@@ -200,12 +212,13 @@ class ToDoActivity : AppCompatActivity() {
         val mediaPlayer = MediaPlayer.create(this, R.raw.ra)
         mediaPlayer.start()
     }
-    private fun hideReward () {
+
+    private fun hideReward() {
         rewardImageView.visibility = View.GONE
     }
 
 
-    fun lockEditing(){
+    fun lockEditing() {
         passCard.apply {
             alpha = 0f
             visibility = View.VISIBLE
@@ -214,10 +227,12 @@ class ToDoActivity : AppCompatActivity() {
 
         unlock.setOnClickListener {
             val pass = editPassword.text.toString()
-            if ( password == pass) {
+            if (password == pass) {
                 passCard.visibility = View.GONE
                 addButton.visibility = View.VISIBLE
                 editPassword.setText("")
+                locked = false
+                Log.d("!!!", " upplåst $locked")
             } else {
                 Toast.makeText(this, "Skriv rätt lösenord! ", Toast.LENGTH_SHORT).show()
             }
@@ -225,10 +240,12 @@ class ToDoActivity : AppCompatActivity() {
 
         lock.setOnClickListener {
             val pass = editPassword.text.toString()
-            if ( password == pass) {
+            if (password == pass) {
                 passCard.visibility = View.GONE
                 addButton.visibility = View.GONE
                 editPassword.setText("")
+                locked = true
+                Log.d("!!!", " låst $locked")
             } else {
                 Toast.makeText(this, "Skriv rätt lösenord! ", Toast.LENGTH_SHORT).show()
             }
