5a8d50c0223c6813c7aec5fe0f9d902e931e5238
zadnan82 <zainab.adnan@iths.se>
2021-10-27 13:26:03 +0200

    Fixat låset

--- app/src/main/java/com/example/helpwithpicturesapp/Users.kt
+++
@@ -1,9 +0,0 @@
-package com.example.helpwithpicturesapp
-
-import com.google.firebase.firestore.DocumentId
-
-data class Users(@DocumentId var userId: String? = null, // här kan man sätta dokument ID. ska vi ha det namnet?
-                   var email: String? = null,
-                   var password: String? = null)
-{
-}
